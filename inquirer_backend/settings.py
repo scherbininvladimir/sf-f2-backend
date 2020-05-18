2280ec400108e644cd37a417730bd587293d0d72
Vladimir Scherbinin <scherbinin.vladimir@gmail.com>
2020-05-18 08:17:51 +0000

    ALOWED_HOST + http://95.165.133.240

--- inquirer_backend/settings.py
+++ inquirer_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["95.165.133.240", "127.0.0.1", "localhost"]
 
 
 # Application definition
